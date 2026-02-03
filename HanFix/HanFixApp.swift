079d97de824ae7d31dcb0cefd86598a19e10196a
woodong-222 <dw2083ehddn@naver.com>
2026-02-03 14:16:13 +0900

    feat: 메뉴창으로 변경

--- HanFix/HanFixApp.swift
+++ HanFix/HanFixApp.swift
@@ -7,8 +7,17 @@ import SwiftUI
 @main
 struct HanFixApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("HanFix", systemImage: "keyboard") {
+            Button("한글 변환 실행") {
+                print("미구현")
+            }
+            
+            Divider() // 구분선
+            
+            Button("종료") {
+                print("앱 종료")
+                NSApplication.shared.terminate(nil)
+            }
         }
     }
 }
